99a265bf64d6f7391c2713689baeb99b5e426c11
a._-0 <a._-0@139.com>
2023-04-03 17:36:23 +0800

    添加缺少的import

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,6 +3,7 @@ import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Bio from "../components/bio"
 
 const About = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
