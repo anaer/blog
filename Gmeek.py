c8831de3def7b7a79be20c8428c38146e1635a7a
random <a@b.com>
2025-01-07 11:30:22 +0800

    交换蓝绿配色

--- Gmeek.py
+++ Gmeek.py
@@ -241,8 +241,8 @@ class GMEEK():
 
         # 计算 RGB 颜色值 (0 到 255 之间) 不取最大的255 防止过于刺眼
         red = int(255 * cratio)
-        green = int(255 * (1 - cratio))
-        blue = int(255 * (1 - uratio))
+        blue = int(255 * (1 - cratio))
+        green = int(255 * (1 - uratio))
 
         # 转换为十六进制颜色代码
         color_code = f'#{red:02x}{green:02x}{blue:02x}'
