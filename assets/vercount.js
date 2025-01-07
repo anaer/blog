dfa51678ed38042dbe0a910f9dadc5a1c5fcd7cc
random <a@b.com>
2025-01-07 18:00:38 +0800

    调整样式 眼睛位置

--- assets/vercount.js
+++ assets/vercount.js
@@ -1,7 +1,7 @@
 function createVercount() {
     let postPublish = document.getElementById('postPublish');
     if (postPublish){
-        postPublish.insertAdjacentHTML('beforebegin','<div id="vercount_container_page_pv" style="display:none;float:left;margin-top:8px;font-size:small;">👁<span id="vercount_value_page_pv"></span></div>');
+        postPublish.insertAdjacentHTML('beforebegin','<div id="vercount_container_page_pv" style="display:none;float:left;margin-top:3px;margin-right:5px;font-size:small;">👁<span id="vercount_value_page_pv"></span></div>');
     }
     let runday = document.getElementById('runday');
     runday.insertAdjacentHTML('afterend', '<span id="vercount_container_site_pv" style="display:none">总浏览量<span id="vercount_value_site_pv"></span>次 • </span>');
