d10ff4cce0dbfcf67cb52180922d5e5c00435cab
random <a@b.com>
2025-01-23 16:41:57 +0800

    更新#数字链接处理

--- Gmeek.py
+++ Gmeek.py
@@ -78,10 +78,9 @@ class GMEEK():
         dconfig={"startSite":"","filingNum":"","onePageListNum":15,"commentLabelColor":"#006b75","i18n":"CN","dayTheme":"light","nightTheme":"dark"}
         config=json.loads(open('config.json', 'r', encoding='utf-8').read())
         self.blogBase={**dconfig,**config}.copy()
-        # 清空前 先缓存, 便于处理
-        self.blogBase["cachePostListJson"] = self.blogBase["postListJson"]
         self.blogBase["postListJson"]=json.loads('{}')
         self.blogBase["singeListJson"]=json.loads('{}')
+        self.cachePostListJson=json.loads('{}')
         if self.blogBase["i18n"]=="CN":
             self.i18n=i18nCN
         else:
@@ -380,8 +379,8 @@ class GMEEK():
             print(f"Found number: {issue.title} {match}")
             matchPostNum = "P"+str(match)
             # 因为postListJson每次执行会先清空, 所以使用缓存处理, 与最新数据可能有差异, 但不太影响
-            if matchPostNum in self.blogBase["cachePostListJson"]:
-                content = content.replace(" #"+match+" ", " ["+self.blogBase["cachePostListJson"][matchPostNum]["postTitle"]+"]("+self.blogBase["homeUrl"]+"/"+self.blogBase["cachePostListJson"][matchPostNum]["postUrl"]+") ")
+            if matchPostNum in self.cachePostListJson:
+                content = content.replace(" #"+match+" ", " ["+self.cachePostListJson[matchPostNum]["postTitle"]+"]("+self.blogBase["homeUrl"]+"/"+self.cachePostListJson[matchPostNum]["postUrl"]+") ")
                 # print(content)
 
         f = open(mdPath, 'w', encoding='UTF-8')
@@ -441,20 +440,23 @@ blog=GMEEK(options)
 
 if not os.path.exists("blogBase.json"):
     print("blogBase is not exists, runAll")
+    blog.cachePostListJson = json.loads('{}')
     blog.runAll()
 else:
+    f=open("blogBase.json","r")
+    lastBlogBase=json.loads(f.read())
+    f.close()
+    if "postListJson" in lastBlogBase:
+        blog.cachePostListJson = lastBlogBase["postListJson"]
+    else:
+        blog.cachePostListJson = json.loads('{}')
     if options.issue_number=="0" or options.issue_number=="":
         print("issue_number=='0', runAll")
         blog.runAll()
     else:
-        f=open("blogBase.json","r")
         print("blogBase is exists and issue_number!=0, runOne")
-        blog.blogBase=json.loads(f.read())
-        f.close()
+        blog.blogBase=lastBlogBase
         blog.runOne(options.issue_number)
-
-# 清空缓存
-blog.blogBase["cachePostListJson"]=json.loads('{}')
 
 listFile=open("blogBase.json","w")
 listFile.write(json.dumps(blog.blogBase, indent=4))
